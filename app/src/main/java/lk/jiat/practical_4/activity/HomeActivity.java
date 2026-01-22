0bdb78dea40310676c533618bb70f0b4915316bf
ADMIN <mhdajinas69@gmial.com>
2026-01-22 09:00:03 +0530

    feat(ui) signin and signup design done

---
+++ app/src/main/java/lk/jiat/practical_4/activity/HomeActivity.java
@@ -0,0 +1,30 @@
+package lk.jiat.practical_4.activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import lk.jiat.practical_4.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        textView = findViewById(R.id.textView2);
+        String email = getIntent().getStringExtra("email");
+        if (!email.isEmpty()) {
+            textView.setText("Welcome " + email);
+        }
+    }
+}
--- app/src/main/java/lk/jiat/practical_4/fragment/SignInFragment.java
+++ app/src/main/java/lk/jiat/practical_4/fragment/SignInFragment.java
@@ -1,21 +1,60 @@
 package lk.jiat.practical_4.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import lk.jiat.practical_4.R;
+import lk.jiat.practical_4.activity.HomeActivity;
 
 public class SignInFragment extends Fragment {
+
+    private EditText emailEditText, passwordEditText;
+    private Button signInBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        emailEditText = view.findViewById(R.id.signinFragmentEmailEditText);
+        passwordEditText = view.findViewById(R.id.signinFragmentPasswordEditText);
+        signInBtn = view.findViewById(R.id.signinBtn);
+        signIn();
+    }
+
+    private void signIn(){
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getContext(),"Please fill all th fields",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = new Intent(getContext(), HomeActivity.class);
+                intent.putExtra("email",email);
+                startActivity(intent);
+            }
+        });
     }
 }
